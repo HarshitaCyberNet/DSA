ff9d7292390b9504423204e38db3b21430cd5cae
harshitagupta <Harshitagupta21305@gmail.com>
2025-12-30 18:59:00 +0530

    Day 5 (29 Dec): Password strength checker project in Python

---
+++ day_5_password_strength_checker/README.md
@@ -0,0 +1,14 @@
+DAY 5 – PASSWORD STRENGTH CHECKER (Python)
+
+Features:
+- Checks password length
+- Detects uppercase, lowercase, digits, and symbols
+- Rates password as Weak, Moderate, or Strong
+
+Concepts Used:
+- Regular expressions
+- Conditional logic
+- Cybersecurity fundamentals
+
+Goal:
+Understand basic password security principles.
---
+++ day_5_password_strength_checker/password_checker.py
@@ -0,0 +1,23 @@
+import re
+def check_password_strength(password):
+    strength=0
+    if len(password)>=8:
+        strength +=1
+    if re.search("[A-Z]", password):
+        strength +=1 
+    if re.search("[a-z]", password):
+        strength +=1
+    if re.search("[0-9]", password):
+        strength +=1
+    if re.search("[!@#$%^&*(),.?\":{}|<>]", password):
+        strength +=1
+    if strength<=2:
+        return "weak"
+    elif strength==3:
+        return "moderate"
+    else:
+        return "strong"
+
+password= input("enter your password:")
+result = check_password_strength(password)
+print(f"your password strength is : {result}")
